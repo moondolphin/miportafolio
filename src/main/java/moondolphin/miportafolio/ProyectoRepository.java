50d3120f60b005dedd25b42554a7be9c334bb5a2
Yesica Puerto <yesica_puerto@yahoo.com.ar>
2025-06-29 02:38:39 -0300

    Añadir seccion Fotografia y nueva logica de enlaces

--- src/main/java/moondolphin/miportafolio/ProyectoRepository.java
+++ src/main/java/moondolphin/miportafolio/ProyectoRepository.java
@@ -8,7 +8,17 @@ package moondolphin.miportafolio;
  *
  * @author Administrator
  */
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProyectoRepository extends JpaRepository<Proyecto, Long> {
+    List<Proyecto> findByNombre(String nombre);
+    
+    
+    // MÉTODO NUEVO CON QUERY EXPLÍCITA
+    @Query("SELECT p FROM Proyecto p WHERE p.descripcion LIKE %:termino%")
+    List<Proyecto> buscarPorTerminoEnDescripcion(@Param("termino") String termino);
+
 }
